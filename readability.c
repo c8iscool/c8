14d0a511d700cf1ec2672f36e7765d91b471ef8c
c8iscool <c8iscool@users.noreply.github.com>
2019-09-23 01:17:15 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- readability.c
+++ readability.c
@@ -6,30 +6,24 @@
 #include <math.h>
 
 int main(void)
-{
+{   
+    int letters = 0;
+    int words = 1;
+    int sentences = 0;
+    
     string text = get_string("Text: \n");
     
-    int letters = 0;
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         if (isalpha(text[i]) != 0)
-        letters++;
-    // counts number of alphabetic characters in the string text
-    }
-    
-    int words = 1;
-    for (int i = 0, n = strlen(text); i < n; i++)
-    {
+        letters++; // counts number of alphabetic characters in the string text
+        
         if (text[i] == ' ' && isalpha(text[i + 1] != 0))
-        words++;
-    }    
+        words++;    
     // counts number of spaces in the string, since there is one fewer space than #words words=1
     // checks that the next char after space is an alphabetic letter
 
-    int sentences = 0;
-    for (int i = 0, n = strlen(text); i < n; i++)
-    {
-        if (text[i] == '.' || text[i] == '!')
+        if (text[i] == '.' || text[i] == '!' || text[i] == '?')
         sentences++;
     }
     
@@ -41,10 +35,10 @@ int main(void)
     if (index < 16 && index >= 1)
     printf("Grade %i", index);
     
-    if (index > 16)
+    else if (index > 16)
     printf("Grade 16+");
     
-    if(index < 1)
+    else if(index < 1)
     printf("Before Grade 1");
         
 }
