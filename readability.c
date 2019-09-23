d25d240308cf287cc530624460cd390a04a709b6
c8iscool <c8iscool@users.noreply.github.com>
2019-09-23 01:25:41 +0000

    automated commit by check50 [check50=True] [lang=C.UTF-8]

--- readability.c
+++ readability.c
@@ -16,29 +16,40 @@ int main(void)
     for (int i = 0, n = strlen(text); i < n; i++)
     {
         if (isalpha(text[i]) != 0)
-        letters++; // counts number of alphabetic characters in the string text
+        {
+            letters++; // counts number of alphabetic characters in the string text
+        }
         
         if (text[i] == ' ' && isalpha(text[i + 1] != 0))
-        words++;    
+        {
+            words++;
+        }
     // counts number of spaces in the string, since there is one fewer space than #words words=1
     // checks that the next char after space is an alphabetic letter
 
         if (text[i] == '.' || text[i] == '!' || text[i] == '?')
-        sentences++;
+        {
+            sentences++;
+        }
     }
     
-    float W = 100.0f / words;
+    float W = 100.0 / words;
     float L = (letters * W);
     float S = (sentences * W);
     int index = round((0.0588 * L) - (0.296 * S) - 15.8);
     
     if (index < 16 && index >= 1)
-    printf("Grade %i", index);
+    {
+        printf("Grade %i", index);
+    }
     
-    else if (index > 16)
-    printf("Grade 16+");
+    else if (index >= 16)
+    {
+        printf("Grade 16+");
+    }
     
     else if(index < 1)
-    printf("Before Grade 1");
-        
+    {
+        printf("Before Grade 1");
+    }
 }
