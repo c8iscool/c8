4ddd5c0360747217cba6bcaadb9b4832ba88b45d
c8iscool <c8iscool@users.noreply.github.com>
2019-10-04 19:43:27 +0000

    automated commit by check50 [check50=True]

--- helpers.c
+++ helpers.c
@@ -9,7 +9,7 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     {
         for (int j = 0; j < width - 1; j++)
         {
-            int avg_value = round(image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3;
+            int avg_value = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3);
             image[i][j].rgbtBlue = avg_value;
             image[i][j].rgbtRed = avg_value;
             image[i][j].rgbtGreen = avg_value;
@@ -29,9 +29,9 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
             int originalGreen = image[i][j].rgbtGreen;
             int originalBlue = image[i][j].rgbtBlue;
 
-            int sepiaRed = round(0.393 * originalRed) + round(0.769 * originalGreen) + round(0.189 * originalBlue);
-            int sepiaGreen = round(0.349 * originalRed) + round(0.686 * originalGreen) + round(0.168 * originalBlue);
-            int sepiaBlue = round(0.272 * originalRed) + round(0.534 * originalGreen) + round(0.131 * originalBlue);
+            int sepiaRed = round((0.393 * originalRed) + (0.769 * originalGreen) + (0.189 * originalBlue));
+            int sepiaGreen = round((0.349 * originalRed) + (0.686 * originalGreen) + (0.168 * originalBlue));
+            int sepiaBlue = round((0.272 * originalRed) + (0.534 * originalGreen) + (0.131 * originalBlue));
 
             if (sepiaBlue > 255) // ensures if rgb level is over, makes it to the max
             {
